6a5848f404e822cc7a1e5d7193a4c3d3905bfde1
Loxy Zhou <loxy.zhou@ncs.com.sg>
2021-02-08 00:04:12 +0800

    change folder path

--- MRBookingDemo/MRBookingDemo/settings.py
+++ MRBookingDemo/MRBookingDemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'uo18+u^w4m)rpt@c41%^n+nrpv(iw*k@o6g6$d18pr5iyexk$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
