cc0b0be4edacfe3f71a88399b3df2819c55d5082
Loxy Zhou <loxyzhou@hotmail.com>
2021-02-13 21:29:11 +0800

    Updated static file references

--- MRBookingDemo/MRBookingDemo/settings.py
+++ MRBookingDemo/MRBookingDemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'uo18+u^w4m)rpt@c41%^n+nrpv(iw*k@o6g6$d18pr5iyexk$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES = [
-#     os.path.join(BASE_DIR, 'webapp/static')
-# ]
+STATICFILES = [
+    os.path.join(BASE_DIR, 'webapp/static')
+]
--- webapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from webapp.models import RoomBooking,MeetingRoom,UserInfo
-admin.site.register(UserInfo)
-admin.site.register(MeetingRoom)
-admin.site.register(RoomBooking)
